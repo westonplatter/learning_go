2bb31ef78668f78c4a5f2e8cd947995270ba9874
Weston Platter <westonplatter@gmail.com>
2014-05-04 22:40:33 -0600

    remove sleep calls and report stats

--- dinning_philosophers/simple.go
+++ dinning_philosophers/simple.go
@@ -3,22 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 var workers int = 2
 var resources = []int{0}
+var load = []int{0, 0}
 
 func main() {
-	fmt.Println("starting")
+	fmt.Println("start")
+
 	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second)
 		worker := choose_worker(workers)
 		can_work := resources_available(worker)
 		if can_work {
 			work(worker)
 		}
 	}
+	fmt.Println("done")
+
+	report()
 }
 
 func choose_worker(workers int) int {
@@ -35,8 +38,26 @@ func resources_available(worker int) bool {
 }
 
 func work(worker int) {
-	fmt.Println("worker", worker, "starting work")
 	resources[0] = 1
-	time.Sleep(2 * time.Second)
+	load[worker] += 1
 	resources[0] = 0
+}
+
+func report() {
+	total_jobs := 0
+	for _, worker_jobs := range load {
+		total_jobs += worker_jobs
+	}
+
+	load_percent := []float64{0, 0}
+	for i := 0; i < len(load); i++ {
+		total_jobs := float64(total_jobs)
+		worker_jobs := float64(load[i])
+		worker_percent := worker_jobs / total_jobs
+
+		load_percent[i] = worker_percent
+	}
+
+	fmt.Println("total_jobs", total_jobs)
+	fmt.Println("job distribution", load_percent)
 }
