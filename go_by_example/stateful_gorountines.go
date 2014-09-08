2937959fa92d858694c0d364c98c94593342325c
Weston Platter <westonplatter@gmail.com>
2014-09-07 22:46:09 -0600

    d13 - send reads and writes through go channels.

---
+++ go_by_example/stateful_gorountines.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync/atomic"
+	"time"
+)
+
+type readOp struct {
+	key  int
+	resp chan int
+}
+
+type writeOp struct {
+	key  int
+	val  int
+	resp chan bool
+}
+
+func main() {
+	var ops int64 = 0
+
+	reads := make(chan *readOp)
+	writes := make(chan *writeOp)
+	done_signal := make(chan bool)
+
+	go func() {
+		var state = make(map[int]int)
+		for {
+			select {
+
+			case read := <-reads:
+				read.resp <- state[read.key]
+
+			case write := <-writes:
+				state[write.key] = write.val
+				write.resp <- true
+
+			case <-done_signal:
+				fmt.Println(state)
+			}
+		}
+	}()
+
+	for r := 0; r < 100; r++ {
+		go func() {
+
+			// the "for" keeps the routines going until the main goroutine stops
+			for {
+				read := &readOp{
+					key:  rand.Intn(5),
+					resp: make(chan int)}
+				reads <- read
+				<-read.resp
+				atomic.AddInt64(&ops, 1)
+			}
+		}()
+	}
+
+	for w := 0; w < 10; w++ {
+		go func() {
+
+			// the "for" keeps the routines going until the main goroutine stops
+			for {
+				write := &writeOp{
+					key:  rand.Intn(5),
+					val:  rand.Intn(100),
+					resp: make(chan bool)}
+				writes <- write
+				<-write.resp
+				atomic.AddInt64(&ops, 1)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second * 2)
+
+	done_signal <- true
+
+	opsFinal := atomic.LoadInt64(&ops)
+	fmt.Println("ops:", opsFinal)
+}
